a0c513ead69bfe26eadde9e3bb44a88e7f8ca092
Patrick T. Cronin <ptcesq@gmail.com>
2015-04-06 07:13:39 -0400

    Revised Combined Script and added 250k Samples

--- scripts/combined_dist.R
+++ scripts/combined_dist.R
@@ -1,22 +1,28 @@
 
 # non-gussuain 
 
-set.seed(876)
-sample_count <- 25000
+if (!exists("y")){
+      set.seed(876)
+  
+      group_1 <- rbinom(500000, 1, 0.005)
+      group_2 <- rbinom(100000, 1, 0.66)
+      group_3 <- rep(0, 400000)
+      group_4 <- rbinom(100000, 1, 0.85)
+      group_5 <- rbinom(400000, 1, 0.001)
+      
+      y <- c(group_1, group_2, group_3, group_4, group_5)
+      rm(group_1, group_2, group_3, group_4, group_5)
+      
+      actual_percent = sum(y)/length(y)
+      }
 
 
-group_1 <- rbinom(500000, 1, 0.005)
-group_2 <- rbinom(100000, 1, 0.66)
-group_3 <- rep(0, 400000)
-group_4 <- rbinom(100000, 1, 0.85)
-group_5 <- rbinom(400000, 1, 0.001)
-
-y <- c(group_1, group_2, group_3, group_4, group_5)
-actual_percent = sum(y)/length(y)
+sample_count <- 250000
 
 result <- data.frame(iteration= numeric(), 
                      err=numeric(),
                      group=numeric())
+
 for (i in 1:sample_count) {
   sample_1 <- sample(y, i)
   sample_percent <- sum(sample_1)/i
@@ -29,12 +35,18 @@ colnames(result) <- c("n", "err", "group")
 
 # Non-Random Selection
 
+non_random <- data.frame(iteration= numeric(), 
+                         err=numeric(),
+                         group=numeric())
 
-non_random <- read.csv("C:/Users/Patrick/OneDrive/R Programming/random/data/result.csv")
-non_random <- non_random[,-1]
-colnames(non_random) <- c("n", "err", "class")
-non_random_sub <- non_random[1:sample_count,]
-colnames(non_random_sub) <- c("n", "err", "group")
+for (i in 1:sample_count){
+  sample_1 <- y[1:i]
+  sample_percent <- sum(sample_1)/i
+  err <- abs(actual_percent - sample_percent)/actual_percent
+  gp=2
+  non_random <- rbind(non_random, c(i, err, gp))  
+}
+colnames(non_random) <- c("n", "err", "group")
 
 # Simple Plot 
 
@@ -43,7 +55,7 @@ p <- qplot(n,err, data=result, geom="smooth", color=group, ylim=c(0.00, 1.0),
            main="Relative Error for Ramdom Sample with Non-Gausian Distribution", 
            ylab="Error", xlab="Sample Size")  
 
-p <- p + geom_smooth(aes(n, err), data=non_random_sub, color="blue")
+p <- p + geom_smooth(aes(n, err), data=non_random, color="red")
 
 p
 
